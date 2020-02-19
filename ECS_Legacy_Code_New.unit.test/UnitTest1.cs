7e4a42103ef5e7e6c2d1a6a1a023a70f75537f08
Jonas Bay Andreasen <au616208@uni.au.dk>
2020-02-19 14:50:41 +0100

    Newest version

---
+++ ECS_Legacy_Code_New.unit.test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ECS_Legacy_Code_New.unit.test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
