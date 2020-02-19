09761d54fc6d8167ca9c2267bc9d7ba54b70de56
Alexander Smith Mølholm <au613156@uni.au.dk>
2020-02-19 15:01:37 +0000

    Alexander

--- ECS_Legacy_Code_New.unit.test/UnitTest1.cs
+++ ECS_Legacy_Code_New.unit.test/UnitTest1.cs
@@ -9,21 +9,40 @@ namespace ECS_Legacy_Code_New.unit.test
     public class Tests
     {
         private ECS _uut;
-        private IHeater _heater;
-        private ITempSensor _tempSensor;
+        private fakeHeater _fakeHeater;
+        private fakeTempSensor _fakeTempSensor;
 
         [SetUp]
         public void Setup()
         {
-            _uut = new ECS();
-            _heater = new fakeHeater();
-            _tempSensor = new fakeTempSensor();
+            _fakeHeater = new fakeHeater();
+            _fakeTempSensor = new fakeTempSensor();
+            _uut = new ECS(20, _fakeHeater, _fakeTempSensor);
         }
 
         [Test]
-        public void Test1()
+        public void TestThatECSWillTurnOnHeaterWhenTempIsTooLow()
         {
-            Assert.Pass();
+            _fakeTempSensor.temp = 18;
+            _uut.Regulate();
+            Assert.That(_fakeHeater.TurnedOnCount, Is.EqualTo(1));
+        }
+
+        private class fakeHeater : IHeater
+        {
+            public void TurnOn() { TurnedOnCount++; }
+            public void TurnOff() { TurnedOnCount--; }
+            public bool RunSelfTest() { return true; }
+
+            public int TurnedOnCount = 0;
+        }
+
+        private class fakeTempSensor : ITempSensor
+        {
+            public int GetTemp() { return temp; }
+            public bool RunSelfTest() { return true; }
+
+            public int temp = 20;
         }
     }
 }
